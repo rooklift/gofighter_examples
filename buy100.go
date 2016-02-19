6fc9d41a1623197e68947dd895b1471fcd288d8b
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-02-19 20:27:47 +0000

    Level 1 solution

---
+++ buy100.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/fohristiwhirl/gofighter"		// go get -u github.com/fohristiwhirl/gofighter
+)
+
+func main() {
+
+	// Assuming that start_game.exe has been run, this is all we need...
+
+	levelname := gofighter.NameFromUser()
+	info := gofighter.TradingInfoFromName(levelname)
+
+	order := gofighter.ShortOrder{
+		Direction: "buy",
+		OrderType: "limit",
+		Qty: 100,
+		Price: 10000,
+	}
+
+	result, _ := gofighter.Execute(info, order, nil)
+	gofighter.PrintJSON(result)
+}
