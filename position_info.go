9442b898bc546064c430157a59ebc78632a8db3c
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-02-21 21:47:50 +0000

    Position Info printer

---
+++ position_info.go
@@ -0,0 +1,35 @@
+package main
+
+// This program merely displays constantly-updated position info.
+
+import (
+    "fmt"
+    "time"
+
+    "github.com/fohristiwhirl/gofighter"        // go get -u github.com/fohristiwhirl/gofighter
+)
+
+func main()  {
+
+    // We assume game_start.exe has been called
+
+    levelname := gofighter.NameFromUser()
+    info := gofighter.TradingInfoFromName(levelname)
+
+    position_queries := make(chan chan gofighter.Position)
+    go gofighter.PositionWatch(info, position_queries)
+
+    // How this works: the goroutine PositionWatch uses a WebSocket to keep track of
+    // the position, and when we send a channel to it via its input channel, it sends
+    // a copy of the position back to us along the channel we provided.
+
+    for {
+        position := gofighter.GetPosition(position_queries) // Behind the scenes, this sends a
+                                                            // one-time channel to the goroutine
+
+        fmt.Printf(time.Now().Format("2006-01-02 15:04:05 ... "))
+        fmt.Printf("Shares: %d, Dollars: $%d\n", position.Shares, position.Cents / 100)
+
+        time.Sleep(2 * time.Second)
+    }
+}
