c77a9912591556dfa8aa1d50dc02cdc93635e5c6
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2016-02-21 21:18:02 +0000

    Use new MarketWatch function

--- market_info.go
+++ market_info.go
@@ -16,14 +16,20 @@ func main()  {
     levelname := gofighter.NameFromUser()
     info := gofighter.TradingInfoFromName(levelname)
 
-    var market gofighter.Market
-    market.Init(info, gofighter.Ticker)
+    market_queries := make(chan chan gofighter.Market)
+    go gofighter.MarketWatch(info, market_queries)
+
+    // How this works: the goroutine MarketWatch uses a WebSocket to keep track of
+    // the market, and when we send a channel to it via its input channel, it sends
+    // a copy of the market back to us along the channel we provided.
 
     for {
-        market.Update()
-        t := time.Now()
-        fmt.Printf(t.Format("2006-01-02 15:04:05 ... "))
+        market := gofighter.GetMarket(market_queries)   // Behind the scenes, this sends a
+                                                        // one-time channel to the goroutine
+
+        fmt.Printf(time.Now().Format("2006-01-02 15:04:05 ... "))
         market.Print()
+
         time.Sleep(2 * time.Second)
     }
 }
